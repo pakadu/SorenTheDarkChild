c5f198a6e2df87948dba0d12fafb5c0454cd616e
dusan862020 <86-2020@ftn.edu.rs>
2024-03-18 15:39:42 +0100

    enemy facing direction

--- Assets/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-
     Transform player;
     public float moveSpeed;
+
+    private bool facingRight = false;
 
     void Start()
     {
@@ -16,6 +17,26 @@ public class EnemyMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector2.MoveTowards(transform.position, player.transform.position, moveSpeed * Time.deltaTime);
+        Vector2 moveDirection = (player.position - transform.position).normalized;
+
+        if (moveDirection.x > 0 && !facingRight)
+        {
+            Flip();
+        }
+        else if (moveDirection.x < 0 && facingRight)
+        {
+            Flip();
+        }
+
+        // Move towards the player
+        transform.position = Vector2.MoveTowards(transform.position, player.position, moveSpeed * Time.deltaTime);
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
     }
 }
